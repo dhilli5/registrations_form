07f8b937856f5be625ac9b776ab1784253d8824d
dhiili <narayanavaramdhilli7461.com>
2023-05-06 10:21:48 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+from app.forms import *
+from django.core.mail import send_mail
+
+
+def registration(request):  # sourcery skip: extract-method
+    ufo=UserForm()
+    pfo=ProfileForm()
+    d={'ufo':ufo,'pfo':pfo}
+    
+    if request.method=="POST" and request.FILES:
+        UFD=UserForm(request.POST)
+        PFD=ProfileForm(request.POST,request.FILES)
+        
+        
+        if UFD.is_valid() and PFD.is_valid():
+            NSUO=UFD.save(commit=False)
+            password=UFD.cleaned_data['password']
+            NSUO.set_password(password)
+            NSUO.save()
+            
+            
+            NSPO=PFD.save(commit=False)
+            NSPO.username=NSUO 
+            NSPO.save()  
+            send_mail('wish',
+                      'good morning',
+                      'nvasantha807@gmail.com'
+                      [NSUO.email],
+                      fail_silently=True)
+
+            
+            return HttpResponse("registration successfully")
+        
+        else:
+            return HttpResponse("data not valid")  
+    return render(request,'registration.html',d)       
+    
